03eba1234ed12a14ca5dddb1066bf541d35796a1
mrninhvn <1.ninh.vn@gmail.com>
2020-02-24 00:20:18 +0700

    test 10

--- neopixel.ts
+++ neopixel.ts
@@ -496,12 +496,12 @@ namespace neopixel {
      * @param pin the pin where the neopixel is connected.
      * @param numleds number of leds in the strip, eg: 24,30,60,64
      */
-    //% blockId="neopixel_create" block="All RGB LED as %mode"
+    //% blockId="neopixel_create" block="NeoPixel at pin %pin as %mode"
     //% weight=90 blockGap=8
     //% parts="neopixel"
     //% trackArgs=0,2
     //% blockSetVariable=strip
-    export function create(mode: NeoPixelMode): Strip {
+    export function create(pin: DigitalPin, mode: NeoPixelMode): Strip {
         let strip = new Strip();
         let stride = mode === NeoPixelMode.RGBW ? 4 : 3;
         strip.buf = pins.createBuffer(numleds * stride);
@@ -510,7 +510,7 @@ namespace neopixel {
         strip._mode = mode;
         strip._matrixWidth = 0;
         strip.setBrightness(255)
-        strip.setPin(16)
+        strip.setPin(pin)
         return strip;
     }
 
